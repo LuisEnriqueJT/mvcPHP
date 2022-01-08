533ed6fe1cd369056676743337124c5e22a41e44
LuisEnriqueJT <luisenrique.jt@outlook.com>
2022-01-07 18:56:39 -0600

    Se crea el script usuario.php dentro del fichero models

---
+++ models/usuario.php
@@ -0,0 +1,42 @@
+<?php
+
+	class Usuario{
+		public $nombre;
+		public $apellido;
+		public $email;
+		public $password;
+
+		public function setNombre($nombre){
+			$this->nombre = $nombre;
+		}
+
+		public function setApellido($apellido){
+			$this->apellido = $apellido;
+		}
+
+		public function setEmail($email){
+			$this->email = $email;
+		}
+
+		public function setPassword($password){
+			$this->password = $password;
+		}
+
+		public function getNombre(){
+			return $this->nombre;
+		}
+
+		public function getApellido(){
+			return $this->apellido;
+		}
+
+		public function getEmail(){
+			return $this->email;
+		}
+
+		public function getPassword(){
+			return $this->password;
+		}
+
+	}
+?>
